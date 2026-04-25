2c59cc124cd98112d529088ebafb72a6f54879af
md mohosin <mdmohosin.web@gmail.com>
2026-04-25 19:13:43 +0600

    clone join method

--- src/_/join/join.js
+++ src/_/join/join.js
@@ -1,9 +1,9 @@
-export default function join(array) {
+export default function join(array, separator = ',') {
     let result = ''
     for (let i = 0; i < array.length; i++) {
-        result+=array[i]
+        result += array[i]
         if (i !== array.length - 1) {
-             result+= '_'
+            result += separator
         }
     }
     return result
